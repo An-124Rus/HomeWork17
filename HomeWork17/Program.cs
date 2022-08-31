777da4fbcab93087cf66f0b060442bf0cd113b7d
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-08-31 15:15:59 +0300

    HomeWork17

---
+++ HomeWork17/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int degreeNumber = 2;
+        int degreeCount = 1;
+        long degree = 2;
+        Random number = new Random();
+        int value = number.Next();
+        Console.WriteLine(value);
+
+        while (degree < value)
+        {
+            degreeCount++;
+            degree *= degreeNumber;
+            Console.WriteLine($"{degreeCount}. {degree}");
+        }
+
+        Console.WriteLine($"Минимальная степень двойки равная {degreeCount}" +
+            $" превосходящая случайное число {value} равна {degree}.");
+    }
+}
